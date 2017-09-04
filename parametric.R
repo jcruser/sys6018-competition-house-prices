cee0657911f345c9c0a1a80bc16bbca796dfa48f
boh0suh <boh0suh@gmail.com>
2017-09-04 10:27:30 -0400

    Update parametric.R

--- parametric.R
+++ parametric.R
@@ -200,3 +200,10 @@ mypreds.lm3[757,2] <- 0
 
 #FINAL FIRST PREDICTIONS -- SCORE = 0.25 on Kaggle
 write.table(mypreds.lm3, file = "eih2nn_houses_lm2.csv", row.names=F, sep=",") #Write out to a csv
+
+# Non-parametric Approach
+
+install.packages("class")
+library(class)  # install package class to run knn function 
+
+pred <- knn(train.2, test, train.2$SalePrice, k=3)  # gettig errors of missing value...
